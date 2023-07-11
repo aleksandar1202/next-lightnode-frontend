a4f518520a86b6d49ba7446e9587a3068ae88460
shinyjohnson <ShinyJohnsonn@gmail.com>
2023-07-12 04:28:54 +0800

    updating landing page contents

--- common/constants/ui.ts
+++ common/constants/ui.ts
@@ -50,42 +50,42 @@ export const ARR_BLOGS: Array<LNSimpleBlogArgs> = [{
   side: 'ltr',
   imgSrc:'/assets/images/v1.png',
   noBorder: true,
-  description: 'Lightnode is a semi-centralized staking service that maximizes the interest users can earn on Proof of Stake tokens. Ethereum will be the first asset that LightNode supports.'
-}, {
-  title: 'THE MARKET TODAY',
-  btnTitle: 'Read more',
-  side: 'rtl',
-  imgSrc:'/assets/images/v2.png',
-  noBorder: false,
-  description: 'As Decentralized finance (DeFi) continues to explode, more and more investors are actively adopting innovative mechanisms to earn rewards, mitigate volatility, and multiply their crypto wealth. Staking is one of the most secure and profitable ways to achieve sustained growth while fully retaining the ownership of staked digital assets.'
+  description: 'Lightnode is a simplified and secure tokenized staking service that maximizes the interest users can earn on Proof of Stake tokens. LightNode offers two types of staking services. LightNode Shared and LightNode Solo. Users may also participate in liquidity farming as LightNode will offer additional rewards for those who choose to do so. Ethereum will be the first asset that LightNode supports.'
 }, {
   title: 'LIGHTNODE SHARED',
   btnTitle: 'Learn more',
   side: 'ltr',
   imgSrc:'/assets/images/v3.png',
   noBorder: false,
-  description: 'For users with less than 32 ETH, LightNode Shared offers a network of validators on behalf of stakers who are utilizing the Pool. Once the 32 ETH threshold is met from the collective deposits, a new validator is created and added to LightNode Shared to earn rewards. The stakers are paid out proportionally to their share in the pool.'
+  description: 'For users with less than 32 Ethereum, LightNode Shared offers a network of validators on behalf of users who wish to stake their Ethereum. Once the 32 Ethereum threshold is met from the collective deposits, a new validator is created and added to LightNode Shared to earn rewards. Users are paid out in proportion to their share in the staking pool represented by a 1:1 ratio taken called slETH. This token'
 }, {
   title: 'LIGHTNODE SOLO',
   btnTitle: 'Learn more',
   side: 'rtl',
   imgSrc:'/assets/images/Group-76.png',
   noBorder: false,
-  description: 'For users with greater than 32 ETH, Lightnode Solo intends to make staking and running a personal validator as simple as possible without the difficulties of maintaining and operating your own server. Our semi-centralized approach provides users with greater security and convenience.'
+  description: 'For users with greater than 32 Ethereum, LightNode Solo intends to make staking and running a personal validator as simple as possible without the difficulties of maintaining and operating your own server or hardware. Our semi-centralized approach provides users with greater security and convenience.'
 }, {
-  title: 'ECONOMICS AND SPECIFICATIONS',
-  btnTitle: 'Read more',
+  title: 'WHAT IS SLETH',
+  btnTitle: 'Learn more',
+  side: 'rtl',
+  imgSrc:'/assets/images/v2.png',
+  noBorder: false,
+  description: 'slETH is a 1:1 ratio utility token which is given to a user who deposits Ethereum into LightNode Shared. Since this token is backed by liquidity, users may exchange their slETH back into Ethereum at any time. slETH may additionally be used for voting rights within LightNode DAO.'
+}, {
+  title: 'LIGHTNODE LIQUIDITY FARM',
+  btnTitle: 'Learn more',
   side: 'ltr',
   imgSrc:'/assets/images/v5.png',
   noBorder: false,
-  description: 'The LightNode ecosystem is powered by four different tokens that allow users to access the products and features of the LightNode staking infrastructure'
+  description: 'Users who wish to participate in LightNode Liquidity Farm may do so by adding liquidity to the slETH/ETH pool. LightNode will begin with Uniswap and over time will begin to reach out to additional DeFi protocols.'
 }, {
   title: 'ROADMAP',
   btnTitle: 'Learn more',
   side: 'rtl',
   imgSrc:'/assets/images/v6.png',
   noBorder: false,
-  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  description: 'LightNode aims to deploy to main by the late 2022.'
 }]
 
 export const ARR_VALIDATORS: Array<LNValidatorArgs> = [{
